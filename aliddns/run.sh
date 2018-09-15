b9084e58835d8bdf54d465297aae2e69bae4fa41
Jun <my.fever.sky@gmail.com>
2018-09-15 11:07:13 +0800

    bugfix

--- aliddns/run.sh
+++ aliddns/run.sh
@@ -8,13 +8,14 @@ SECRET=$(jq --raw-output ".accesskey_secret" $CONFIG_PATH)
 DOMAINS=$(jq --raw-output ".domain | length" $CONFIG_PATH)
 REDO=$(jq --raw-output ".redo" $CONFIG_PATH)
 
-while [ 1 ]
+while true
 do
     CURRENT_IP=$(./aliddns_arm7 getip | grep -E -o '([0-9]+\.){3}[0-9]+')
+    echo current ip is $CURRENT_IP, updating ...
     if [ "$DOMAINS" -gt "0" ]; then
         for (( i=0; i < "$DOMAINS"; i++ )); do
             DOMAIN=$(jq --raw-output ".domain[$i]" $CONFIG_PATH)
-            exec ./aliddns_arm7 --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
+            ./aliddns_arm7 --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
         done
     fi
     sleep ${REDO}
