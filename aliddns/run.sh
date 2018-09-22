187d5c4449eb3df090525988a163730dd5a7fe1f
Jun <my.fever.sky@gmail.com>
2018-09-22 08:53:32 +0800

    ignore exceptions

--- aliddns/run.sh
+++ aliddns/run.sh
@@ -10,13 +10,20 @@ REDO=$(jq --raw-output ".redo" $CONFIG_PATH)
 
 while true
 do
+    set +e
     CURRENT_IP=$(./aliddns_arm7 getip | grep -E -o '([0-9]+\.){3}[0-9]+')
-    echo current ip is $CURRENT_IP, updating ...
-    if [ "$DOMAINS" -gt "0" ]; then
-        for (( i=0; i < "$DOMAINS"; i++ )); do
-            DOMAIN=$(jq --raw-output ".domain[$i]" $CONFIG_PATH)
-            ./aliddns_arm7 --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
-        done
+    if [ $? -eq 0 ]; then
+        echo current ip is $CURRENT_IP, updating ...
+        
+        if [ "$DOMAINS" -gt "0" ]; then
+            for (( i=0; i < "$DOMAINS"; i++ )); do
+                DOMAIN=$(jq --raw-output ".domain[$i]" $CONFIG_PATH)
+                ./aliddns_arm7 --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
+            done
+        fi
+        sleep ${REDO}
+    else        
+        sleep 5
     fi
-    sleep ${REDO}
+    set -e
 done
