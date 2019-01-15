b2546f26a61bd4c1d961ff38306e16a96ddd01c2
Jun <my.fever.sky@gmail.com>
2019-01-15 23:01:39 +0800

    added support for amd64

--- aliddns/run.sh
+++ aliddns/run.sh
@@ -11,14 +11,14 @@ REDO=$(jq --raw-output ".redo" $CONFIG_PATH)
 while true
 do
     set +e
-    CURRENT_IP=$(./aliddns_arm7 getip | grep -E -o '([0-9]+\.){3}[0-9]+')
+    CURRENT_IP=$(aliyun-ddns-cli getip | grep -E -o '([0-9]+\.){3}[0-9]+')
     if [ $? -eq 0 ]; then
         echo current ip is $CURRENT_IP, updating ...
         
         if [ "$DOMAINS" -gt "0" ]; then
             for (( i=0; i < "$DOMAINS"; i++ )); do
                 DOMAIN=$(jq --raw-output ".domain[$i]" $CONFIG_PATH)
-                ./aliddns_arm7 --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
+                aliyun-ddns-cli --id ${ID} --secret ${SECRET} update -d ${DOMAIN} -i ${CURRENT_IP}
             done
         fi
         sleep ${REDO}
